9888c5bb63e5384588967d9e70422a368aecaa0b
Andre Demavivas <andredemavivas@gmail.com>
2024-01-11 01:13:53 +0800

    initial: added blog data and theme

---
+++ src/constants/index.ts
@@ -0,0 +1,27 @@
+import img_1 from '../assets/images/component-02/Image-01.jpg'
+import img_2 from '../assets/images/component-02/Image-02.jpg'
+import img_3 from '../assets/images/component-02/Image-03.jpg'
+
+export const blogs = [
+  {
+    id: 1,
+    image: img_1,
+    title: 'Summer Lunch Menu By Mark Best',
+    description:
+      "AEG ambassador Mark Best's summer eats are guaranteed to help you make the most of the warmer weather and entertaining at home.",
+  },
+  {
+    id: 2,
+    image: img_2,
+    title: 'A Traditional Chrismats Eve, Mark Best Style',
+    description:
+      "One of Australia's best chefs and AEG ambassador, Mark Best, shares his favourite Christmas Eve menu which is sure to impress your guests.",
+  },
+  {
+    id: 3,
+    image: img_3,
+    title: 'Taking Taste Further',
+    description:
+      "This exclusive cookbook gives you all the know-how you need. We've designed it to make sure you get the most of our products - and the best out of your food.",
+  },
+]
---
+++ src/styles/theme.ts
@@ -0,0 +1,8 @@
+export const theme = {
+  bg: {
+    primary: '#0f1414',
+  },
+  color: {
+    red: '#c51430',
+  },
+}
